a5de5805e4c3582cf396adcfe6a485524e414c99
Ivan Sevcik <iv.sevcik@gmail.com>
2016-12-01 22:41:58 +0100

    cards definition

---
+++ app/cards-definition.ts
@@ -0,0 +1,123 @@
+// generated with http://www.json-generator.com
+export const cardsDefinition = [
+  {
+    "name": "Nolan",
+    "health": 5,
+    "strengh": 6,
+    "index": 0
+  },
+  {
+    "name": "Maddox",
+    "health": 10,
+    "strengh": 1,
+    "index": 1
+  },
+  {
+    "name": "Rose",
+    "health": 6,
+    "strengh": 3,
+    "index": 2
+  },
+  {
+    "name": "Clemons",
+    "health": 6,
+    "strengh": 7,
+    "index": 3
+  },
+  {
+    "name": "Perez",
+    "health": 9,
+    "strengh": 2,
+    "index": 4
+  },
+  {
+    "name": "Calhoun",
+    "health": 7,
+    "strengh": 4,
+    "index": 5
+  },
+  {
+    "name": "Love",
+    "health": 8,
+    "strengh": 2,
+    "index": 6
+  },
+  {
+    "name": "Hodge",
+    "health": 8,
+    "strengh": 7,
+    "index": 7
+  },
+  {
+    "name": "White",
+    "health": 1,
+    "strengh": 4,
+    "index": 8
+  },
+  {
+    "name": "Vargas",
+    "health": 1,
+    "strengh": 2,
+    "index": 9
+  },
+  {
+    "name": "Mayer",
+    "health": 8,
+    "strengh": 1,
+    "index": 10
+  },
+  {
+    "name": "Bradshaw",
+    "health": 3,
+    "strengh": 5,
+    "index": 11
+  },
+  {
+    "name": "Espinoza",
+    "health": 3,
+    "strengh": 5,
+    "index": 12
+  },
+  {
+    "name": "Camacho",
+    "health": 6,
+    "strengh": 8,
+    "index": 13
+  },
+  {
+    "name": "Burns",
+    "health": 5,
+    "strengh": 9,
+    "index": 14
+  },
+  {
+    "name": "Mcmahon",
+    "health": 5,
+    "strengh": 3,
+    "index": 15
+  },
+  {
+    "name": "Kane",
+    "health": 2,
+    "strengh": 6,
+    "index": 16
+  },
+  {
+    "name": "Charles",
+    "health": 9,
+    "strengh": 10,
+    "index": 17
+  },
+  {
+    "name": "Craig",
+    "health": 7,
+    "strengh": 10,
+    "index": 18
+  },
+  {
+    "name": "Holcomb",
+    "health": 1,
+    "strengh": 8,
+    "index": 19
+  }
+];
