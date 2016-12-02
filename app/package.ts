e69d3cb8c9ba49bcff6475240552c612821aefc7
Ivan Sevcik <iv.sevcik@gmail.com>
2016-12-02 12:47:36 +0100

    Package model

--- app/package.ts
+++ app/package.ts
@@ -1,3 +1,16 @@
+import { Card } from './card';
+import { cardsDefinition } from './cards-definition';
+
 export class Package {
-	
-};
+	name: string;
+	description: string;
+	tags: string[];
+
+	includedCards: Card[];
+	get excludedCards(): Card[] {
+		var self = this;
+		return cardsDefinition.filter(function (dcard: Card) {
+			return self.includedCards.findIndex(function (icard: Card) { return icard.Index === dcard.Index }) == -1;
+		});
+	}
+}
